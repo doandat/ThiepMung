2443ffed81f0cbeb0f1e248300e6040ac8141d0b
doandat <doanvandat93@gmail.com>
2016-03-03 10:00:14 +0700

    Create project

---
+++ ThiepMung/ThiepMung/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThiepMung
+//
+//  Created by DatDV on 3/3/16.
+//  Copyright (c) 2016 Amobi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
